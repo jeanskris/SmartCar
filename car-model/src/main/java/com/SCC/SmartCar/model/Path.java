a9166ce51ed9362c8b6dc95e72a2ed73335c072e
jeanskris <823509241@qq.com>
2016-07-27 10:43:58 +0800

    Path Model

---
+++ car-model/src/main/java/com/SCC/SmartCar/model/Path.java
@@ -0,0 +1,41 @@
+package com.SCC.SmartCar.model;
+
+import org.springframework.stereotype.Repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by ZJDX on 2016/7/27.
+ */
+@Repository
+public class Path implements Serializable {
+    private int PathId;
+    private int carId;
+    private List<Coordinate> points;
+    public int getCarId() {
+        return carId;
+    }
+
+    public void setCarId(int carId) {
+        this.carId = carId;
+    }
+
+    public int getPathId() {
+        return PathId;
+    }
+
+    public void setPathId(int pathId) {
+        PathId = pathId;
+    }
+
+    public List<Coordinate> getPoints() {
+        return points;
+    }
+
+    public void setPoints(List<Coordinate> points) {
+        this.points = points;
+    }
+
+
+}
