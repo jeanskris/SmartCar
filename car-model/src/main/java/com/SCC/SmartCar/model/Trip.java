53d89ae89c19a75772c11ddcf40497327d29c74e
jeanskris <823509241@qq.com>
2016-07-27 21:53:09 +0800

    add carId

--- car-model/src/main/java/com/SCC/SmartCar/model/Trip.java
+++ car-model/src/main/java/com/SCC/SmartCar/model/Trip.java
@@ -1,12 +1,14 @@
 package com.SCC.SmartCar.model;
 
-import java.io.Serializable;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by ZJDX on 2016/7/25.
  */
-public class Trip implements Serializable {
+@Repository
+public class Trip {
     private int MapID;
+    private int carId;
     private Coordinate startPoint;
     private Coordinate endPoint;
     public int getMapID() {
@@ -32,8 +34,15 @@ public class Trip implements Serializable {
         this.startPoint = startPoint;
     }
 
+    public int getCarId() {
+        return carId;
+    }
+
+    public void setCarId(int carId) {
+        this.carId = carId;
+    }
     public String toString(){
-        return  "mapID:"+this.getMapID()+"startPoint:"+startPoint.getX()+" "+startPoint.getY()+" endPoint:"+endPoint.getX()+" "+endPoint.getY();
+        return  "carId:"+this.getCarId()+"mapID:"+this.getMapID()+"startPoint:"+startPoint.getX()+" "+startPoint.getY()+" endPoint:"+endPoint.getX()+" "+endPoint.getY();
 
     }
 }
